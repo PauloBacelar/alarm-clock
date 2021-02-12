752e2856c8b82736388d6a226fed0dbef27f5c10
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-02-12 16:33:23 -0300

    Created 'toMilliseconds()' and 'getCurrentTime()' functions

--- script.js
+++ script.js
@@ -26,14 +26,47 @@ function showErrorMessage() {
     result.innerHTML = "You're trying to set an alarm with invalid inputs, or alarm is already set.<br/> Refresh the page and try again";
 }
 
+function toMilliseconds(hours, minutes, isExactTime) {
+    if(isExactTime) {
+        let [currentHour, currentMinutes] = [...getCurrentTime()];
+
+        if(currentHour > hours) {
+            return (hours * 3600000 + minutes * 60000) + ((23 - currentHour) * 3600000 + (59 - currentMinutes) * 60000);
+        } else if(hours > currentHour) {
+            if(currentMinutes > minutes) {
+                hours--;
+                minutes += 60;
+            }
+
+            return (hours - currentHour) * 3600000 + (minutes - currentMinutes) * 60000;
+        } else {
+            if(currentMinutes > minutes) {
+                return 23 * 3600000 + (60 - (currentMinutes - minutes)) * 60000;
+            } else {
+                return (minutes - currentMinutes) * 60000;
+            }
+        }
+    } else {
+        return hours * 3600000 + minutes * 60000;
+    }
+}
+
+function getCurrentTime() {
+    // Returns current time in [hours, minutes] format
+    let date = new Date();
+    return [date.getHours(), date.getMinutes()];
+}
+
 // Main function
 button.addEventListener("click", function() {
     let [hours, minutes] = [...getInputs()];
     
-    if(!checkInputs(hours, minutes) || alarmIsSet) {
+    if(!checkInputs(hours, minutes)){ //|| alarmIsSet) {
         showErrorMessage();
         return;
     }
 
     alarmIsSet = true;
+    let millisecondsToPlay = toMilliseconds(hours, minutes, exactTime.checked);
+    console.log(millisecondsToPlay);
 })
