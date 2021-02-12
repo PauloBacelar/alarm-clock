742f5a57d13d308f2acad5332e46b741911ce99e
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-02-12 08:33:06 -0300

    Finished with change-text.js

---
+++ change-text.js
@@ -0,0 +1,13 @@
+// Global variables
+const exactTime = document.querySelector("input#exact-time");
+const afterSomeTime = document.querySelector("input#after-some-time");
+const textToChange = document.querySelector("h2#play-at");
+
+// Functions
+exactTime.addEventListener("click", function(){
+    textToChange.textContent = "Play at"
+})
+
+afterSomeTime.addEventListener("click",function() {
+    textToChange.textContent = "Play after"
+})
